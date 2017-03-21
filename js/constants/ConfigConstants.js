0503fbb8b4797787b683281abead94bac6e6dd35
Raman Alesiankou <romanalesenkov@gmail.com>
2017-03-21 11:40:44 +0300

    change base url

--- js/constants/ConfigConstants.js
+++ js/constants/ConfigConstants.js
@@ -1,5 +1,6 @@
 let ConfigConstants = {
-  BASE_URL: 'https://floating-dusk-14900.herokuapp.com/api',
+  BASE_URL: 'http://user-app-team.herokuapp.com/api',
+  //BASE_URL: 'https://floating-dusk-14900.herokuapp.com/api',
   //BASE_URL: 'http://localhost:8080/api',
 
   setBaseUrl (baseUrl) {
